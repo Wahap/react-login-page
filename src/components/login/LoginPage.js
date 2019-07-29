51c9f3cd97f6e73e8983bc7dc7b68a6d396ac65b
Demir, Abdulvahap <Abdulvahap.Demir@mandg.com>
2019-07-29 12:51:04 +0100

    basic login page added

--- src/components/login/LoginPage.js
+++ src/components/login/LoginPage.js
@@ -1,61 +1,65 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Button, FormGroup, FormControl, FormLabel } from "react-bootstrap";
 class LoginPage extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { myvalue: "test" };
+    this.state = {
+      email: "demir.vahap@gmail.com",
+      password: "asd"
+    };
   }
-  handleSubmit(e) {
-    e.preventDefault();
-    this.setState = { myvalue: "Oh yeah" };
+
+  validateForm() {
+    return this.state.email.length > 0 && this.state.password.length > 0;
   }
+
+  handleChange = event => {
+    this.setState({
+      [event.target.id]: event.target.value
+    });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    const { email, password } = this.state;
+
+    // stop here if form is invalid
+    if (!(email && password)) {
+      return;
+    }
+    this.props.history.push("home/HomePage");
+  };
 
   render() {
     return (
-      <div class="text-center" styles="padding:50px">
-        <div class="logo">login</div>
-        <div class="login-form-1">
-          <form id="login-form" class="text-left">
-            <div class="login-form-main-message" />
-            <div class="main-login-form">
-              <div class="login-group">
-                <div class="form-group">
-                  <label for="lg_username" class="sr-only">
-                    Username
-                  </label>
-                  <input
-                    type="text"
-                    class="form-control"
-                    id="lg_username"
-                    name="lg_username"
-                    placeholder="username"
-                  />
-                </div>
-                <div class="form-group">
-                  <label for="lg_password" class="sr-only">
-                    Password
-                    {this.state.myvalue}
-                    {this.props.myvalue}
-                  </label>
-                  <input
-                    type="password"
-                    class="form-control"
-                    id="lg_password"
-                    name="lg_password"
-                    placeholder="password"
-                  />
-                </div>
-                <div class="form-group login-group-checkbox">
-                  <input type="checkbox" id="lg_remember" name="lg_remember" />
-                  <label for="lg_remember">remember</label>
-                </div>
-              </div>
-              <button onClick="handleSubmit" class="login-button">
-                <i class="fa fa-chevron-right" />
-              </button>
-            </div>
-          </form>
-        </div>
+      <div className="Login">
+        <form onSubmit={this.handleSubmit}>
+          <FormGroup controlId="email" bsSize="large">
+            <FormLabel>Email</FormLabel>
+            <FormControl
+              autoFocus
+              type="email"
+              value={this.state.email}
+              onChange={this.handleChange}
+            />
+          </FormGroup>
+          <FormGroup controlId="password" bsSize="large">
+            <FormLabel>Password</FormLabel>
+            <FormControl
+              value={this.state.password}
+              onChange={this.handleChange}
+              type="password"
+            />
+          </FormGroup>
+          <Button
+            block
+            bsSize="large"
+            disabled={!this.validateForm()}
+            type="submit"
+          >
+            Login
+          </Button>
+        </form>
       </div>
     );
   }
